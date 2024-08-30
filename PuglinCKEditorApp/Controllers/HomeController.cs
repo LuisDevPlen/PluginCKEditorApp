deac4e6ab8cd406b5f26aa11d2efec0727223508
Luis <luisaugustoalves13@gmail.com>
2024-08-30 06:47:58 -0300

    Commit

---
+++ PuglinCKEditorApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PuglinCKEditorApp.Controllers
+{
+
+        public class HomeController : Controller
+        {
+            //
+            // GET: /Home/
+
+            public ActionResult Index()
+            {
+                return View();
+            }
+        }
+    }
---
+++ PuglinCKEditorApp/wwwroot/js/blockquote.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    ClassicEditor
+        .create(document.querySelector('#editor'))
+        .then(editor => {
+            const citationBtn = document.getElementById('citation-btn');
+            citationBtn.addEventListener('click', () => {
+                editor.model.change(writer => {
+                    const citationHtml = '<blockquote class="custom-citation"><cite>Citação...</cite></blockquote>';
+                    const viewFragment = editor.data.processor.toView(citationHtml);
+                    const modelFragment = editor.data.toModel(viewFragment);
+                    editor.model.insertContent(modelFragment, editor.model.document.selection);
+                });
+            });
+        })
+        .catch(error => {
+            console.error('Error initializing CKEditor:', error);
+        });
+});
